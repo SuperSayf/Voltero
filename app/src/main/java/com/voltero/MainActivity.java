f7ce3bc3f983918415170b70f20ed2d728ef54f3
Super Sayf <sayfullah.jumoorty@live.com>
2022-05-14 22:30:07 +0200

    Added test login page

--- app/src/main/java/com/voltero/MainActivity.java
+++ app/src/main/java/com/voltero/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import java.util.Objects;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -19,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogin(View v) {
+        setContentView(R.layout.activity_login);
     }
 
     public void doRegistration(View v) {
@@ -36,16 +41,20 @@ public class MainActivity extends AppCompatActivity {
                     TextView txtEmail = findViewById(R.id.txtEmail);
                     TextView txtPassword = findViewById(R.id.txtPassword);
                     TextView txtAddress = findViewById(R.id.txtAddress);
+                    TextView txtCell = findViewById(R.id.txtCell);
 
-                    // todo: Hash the password
+                    // Hash the password
+                    String password = txtPassword.getText().toString();
+                    String bcryptedPassword = BCrypt.withDefaults().hashToString(12, password.toCharArray());
 
                     // Add the values to the url
                     urlBuilder.addQueryParameter("user_firstname", txtFirstName.getText().toString());
                     urlBuilder.addQueryParameter("user_surname", txtSurname.getText().toString());
                     urlBuilder.addQueryParameter("user_email", txtEmail.getText().toString());
-                    urlBuilder.addQueryParameter("user_password", txtPassword.getText().toString());
+                    urlBuilder.addQueryParameter("user_password", bcryptedPassword);
                     urlBuilder.addQueryParameter("user_address", txtAddress.getText().toString());
                     urlBuilder.addQueryParameter("user_type", "1");
+                    urlBuilder.addQueryParameter("user_cell", txtCell.getText().toString());
 
                     // Build the url
                     String url = urlBuilder.build().toString();
@@ -61,6 +70,52 @@ public class MainActivity extends AppCompatActivity {
                                       @Override
                                       public void run() {
                                           TextView textView = findViewById(R.id.txtOutput);
+                                          textView.setText(result);
+                                      }
+                                  }
+                    );
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void doLogin(View v) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+
+                    HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse("https://lamp.ms.wits.ac.za/~s2430888/userLogin.php")).newBuilder();
+
+                    // Get TextView txtEmail and txtPassword from the layout
+                    TextView txtEmail = findViewById(R.id.txtLoginEmail);
+                    TextView txtPassword = findViewById(R.id.txtLoginPassword);
+
+                    // Hash the password
+                    String password = txtPassword.getText().toString();
+                    String bcryptedPassword = BCrypt.withDefaults().hashToString(12, password.toCharArray());
+
+                    // Add the values to the url
+                    urlBuilder.addQueryParameter("user_email", txtEmail.getText().toString());
+                    urlBuilder.addQueryParameter("user_password", bcryptedPassword);
+
+                    // Build the url
+                    String url = urlBuilder.build().toString();
+
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .build();
+
+                    Response response = client.newCall(request).execute();
+                    final String result = Objects.requireNonNull(response.body()).string();
+
+                    runOnUiThread(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          TextView textView = findViewById(R.id.txtLoginOutput);
                                           textView.setText(result);
                                       }
                                   }
