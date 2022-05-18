523eba2fec1199e1240591a55b6f1500ef1da28b
akarolia47 <abdullahkarolia47@gmail.com>
2022-05-19 01:33:28 +0200

    cardview template + activities added
    ShopperHome + Categories XML added
    cardView template and supporting classes added

---
+++ app/src/main/java/com/voltero/Categories.java
@@ -0,0 +1,14 @@
+package com.voltero;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Categories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+}
