ec03563eb7c9c22f4241b1f9d0123e49f9ed59ac
Mu'aaz Bassa <muaazbassa@gmail.com>
2022-05-20 00:27:14 +0200

    Spinner Colour Changed
    We can now see the text!!!

--- app/src/main/java/com/voltero/Register.java
+++ app/src/main/java/com/voltero/Register.java
@@ -26,7 +26,7 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
 
         //Setup spinner
         Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.spinner_items, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.spinner_items, R.layout.my_selected_item_spinner);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
