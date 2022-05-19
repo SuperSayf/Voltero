4f595fb8a4f50a3a78338783a5318b2e37f65d1d
Super Sayf <sayfullah.jumoorty@live.com>
2022-05-19 16:15:24 +0200

    Added all register validation

--- app/src/main/java/com/voltero/MainActivity.java
+++ app/src/main/java/com/voltero/MainActivity.java
@@ -50,49 +50,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void doRegistration(View v) {
-        // Get TextView txtFirstName, txtSurname, txtEmail, txtPassword and txtAddress from the layout
-        TextView txtFirstName = findViewById(R.id.txtFirstName);
-        TextView txtSurname = findViewById(R.id.txtSurname);
-        TextView txtEmail = findViewById(R.id.txtEmail);
-        TextView txtPassword = findViewById(R.id.txtPassword);
-        TextView txtAddress = findViewById(R.id.txtAddress);
-        TextView txtCell = findViewById(R.id.txtCell);
-
-        // Hash the password
-        String password = txtPassword.getText().toString();
-        String bPassword = BCrypt.withDefaults().hashToString(12, password.toCharArray());
-
-        // Create the ContentValues
-        ContentValues params = new ContentValues();
-        params.put("user_firstname", txtFirstName.getText().toString());
-        params.put("user_surname", txtSurname.getText().toString());
-        params.put("user_email", txtEmail.getText().toString());
-        params.put("user_password", bPassword);
-        params.put("user_address", txtAddress.getText().toString());
-        params.put("user_type", "0");
-        params.put("user_cell", txtCell.getText().toString());
-
-        // Create the request
-        Requests.request(this, "addUser", params, response -> {
-            try {
-                // Get the response
-                JSONObject jsonObject = new JSONObject(response);
-
-                if (jsonObject.getString("success").equals("true")) {
-                    // Show the activity_main and close this activity
-                    Intent intent = new Intent(this, MainActivity.class);
-                    startActivity(intent);
-                } else {
-                    // Show the error
-                    Requests.showMessage(this, jsonObject.getString("message"));
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        });
-    }
-
     public void doLogin(View v) {
         ContentValues params = new ContentValues();
         params.put("user_email", ((TextView) findViewById(R.id.txtLoginEmail)).getText().toString());
