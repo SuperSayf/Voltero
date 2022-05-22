935a15b767f4102458ea67da3130938a1cf9ee67
Super Sayf <sayfullah.jumoorty@live.com>
2022-05-22 23:40:40 +0200

    Volunteer Menu working + xml Icons

---
+++ app/src/main/java/com/voltero/Volunteer_Home_Fragment.java
@@ -0,0 +1,144 @@
+package com.voltero;
+
+import android.content.ContentValues;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Volunteer_Home_Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Volunteer_Home_Fragment extends Fragment {
+
+    private RecyclerView courseRV;
+
+    // Arraylist for storing data
+    private ArrayList<CardBuilder> cardBuilderArrayList;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Volunteer_Home_Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Volunteer_Home_Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Volunteer_Home_Fragment newInstance(String param1, String param2) {
+        Volunteer_Home_Fragment fragment = new Volunteer_Home_Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_volunteer__home_, container, false);
+
+        ContentValues params = new ContentValues();
+
+        Requests.request(getActivity(), "getGroceries", params, response -> {
+            try {
+                //TODO: change categories
+                JSONArray categories = new JSONArray(response);
+                courseRV = view.findViewById(R.id.idRVCourse);
+                cardBuilderArrayList = new ArrayList<>();
+                for (int i = 0; i < categories.length(); ++i) {
+                    JSONObject object = categories.getJSONObject(i);
+                    String grc_name = object.getString("grc_name");
+                    String grc_image = object.getString("grc_image");
+                    cardBuilderArrayList.add(new CardBuilder(grc_name, grc_image));
+                }
+                if (isAdded()) {
+                    requireActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            courseRV.setHasFixedSize(true);
+                            LinearCardListMaker linearCardListMaker = new LinearCardListMaker(getActivity(), cardBuilderArrayList);
+                            courseRV.setLayoutManager(new LinearLayoutManager(getActivity()));
+                            courseRV.setAdapter(linearCardListMaker);
+                        }
+                    });
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        return view;
+    }
+
+    public void addNewCard() {
+        ContentValues params = new ContentValues();
+
+        Requests.request(getActivity(), "getGroceries", params, response -> {
+            try {
+                //TODO: change categories
+                JSONArray categories = new JSONArray(response);
+                courseRV = requireView().findViewById(R.id.idRVCourse);
+                cardBuilderArrayList = new ArrayList<>();
+                for (int i = 0; i < categories.length(); ++i) {
+                    JSONObject object = categories.getJSONObject(i);
+                    String grc_name = object.getString("grc_name");
+                    String grc_image = object.getString("grc_image");
+                    cardBuilderArrayList.add(new CardBuilder(grc_name, grc_image));
+                }
+                requireActivity().runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        LinearCardListMaker linearCardListMaker = new LinearCardListMaker(getActivity(), cardBuilderArrayList);
+                        courseRV.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+                        courseRV.setAdapter(linearCardListMaker);
+                    }
+                });
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        });
+    }
+}
