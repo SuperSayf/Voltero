a2bf938f5dface340fedf9efbdc483981ec417f5
Super Sayf <sayfullah.jumoorty@live.com>
2022-05-29 00:19:55 +0200

    Fixed location perms pop up

--- app/src/main/java/com/voltero/MainActivity.java
+++ app/src/main/java/com/voltero/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.ContentValues;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
