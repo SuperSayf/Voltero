9f872f048bd10adf8d90148596881f9c89a1ac07
akarolia47 <abdullahkarolia47@gmail.com>
2022-05-28 07:58:31 +0200

    Cart resfreshes...sorta

--- app/src/main/java/com/voltero/CartListMaker.java
+++ app/src/main/java/com/voltero/CartListMaker.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -48,6 +49,68 @@ public class CartListMaker extends RecyclerView.Adapter<CartListMaker.Viewholder
                 .placeholder(android.R.drawable.screen_background_light_transparent)
                 .resize(500, 500).centerCrop()
                 .into(holder.courseIV);
+
+        HomeShopper.grocery_name = model.getCourse_name();
+        HomeShopper.grocery_image = model.getCourse_image();
+
+        ContentValues params = new ContentValues();
+        params.put("user_email", MainActivity.user_email);
+        params.put("grc_name", HomeShopper.grocery_name);
+        params.put("grc_image", HomeShopper.grocery_image);
+
+        holder.increase.setOnClickListener(v -> {
+
+            AppCompatActivity activity = (AppCompatActivity) v.getContext();
+
+            params.put("change_type", "add");
+
+            Requests.request(activity, "cartItems", params, response -> {
+                //This shit was my last attempt at refreshing the page
+                Fragment fragment = new Shopper_Cart_Fragment();
+                activity.getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.nav_host_fragment_container, fragment, null)
+                        .commit();
+            });
+
+
+
+        });
+
+        holder.decrease.setOnClickListener(v -> {
+
+            AppCompatActivity activity = (AppCompatActivity) v.getContext();
+
+            params.put("change_type", "decrease");
+
+            Requests.request(activity, "cartItems", params, response -> {
+                Fragment fragment = new Shopper_Cart_Fragment();
+                activity.getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.nav_host_fragment_container, fragment, null)
+                        .commit();
+            });
+
+            //This shit was my last attempt at refreshing the page
+
+        });
+
+        holder.remove.setOnClickListener(v -> {
+
+            AppCompatActivity activity = (AppCompatActivity) v.getContext();
+
+            params.put("change_type", "removeAll");
+
+            Requests.request(activity, "cartItems", params, response -> {
+                //This shit was my last attempt at refreshing the page
+                Fragment fragment = new Shopper_Cart_Fragment();
+                activity.getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.nav_host_fragment_container, fragment, null)
+                        .commit();
+
+            });
+        });
     }
 
     @Override
@@ -58,7 +121,9 @@ public class CartListMaker extends RecyclerView.Adapter<CartListMaker.Viewholder
         private TextView courseNameTV;
         private TextView courseQuantity;
         private CardView cardView;
-
+        private ImageButton increase;
+        private ImageButton decrease;
+        private ImageButton remove;
 
 
         public Viewholder(@NonNull View itemView) {
@@ -67,7 +132,9 @@ public class CartListMaker extends RecyclerView.Adapter<CartListMaker.Viewholder
             courseNameTV = itemView.findViewById(R.id.idTVCourseName);
             courseQuantity = itemView.findViewById(R.id.idCourseQuantity);
             cardView = itemView.findViewById(R.id.groceryCard);
-
+            increase = itemView.findViewById(R.id.increase);
+            decrease = itemView.findViewById(R.id.decrease);
+            remove = itemView.findViewById(R.id.remove);
         }
     }
 }
