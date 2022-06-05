3953b772d874dbc17f8394b89d300fc499593f13
Super Sayf <sayfullah.jumoorty@live.com>
2022-06-05 18:16:10 +0200

    Connected everything together
    HomeShopper now searches if it's session has been accepted once post order is clicked, and chat now works dynamically

--- app/src/main/java/com/voltero/HomeShopper.java
+++ app/src/main/java/com/voltero/HomeShopper.java
@@ -1,29 +1,22 @@
 package com.voltero;
 
-import android.app.Activity;
 import android.content.ContentValues;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.hosseiniseyro.apprating.listener.RatingDialogListener;
@@ -35,7 +28,6 @@ import org.json.JSONObject;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -45,7 +37,6 @@ import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import render.animations.Attention;
 import render.animations.Bounce;
 import render.animations.Render;
 import timber.log.Timber;
@@ -75,10 +66,15 @@ public class HomeShopper extends AppCompatActivity implements RatingDialogListen
     // Global variables
     public static List<JSONObject> messagesList = new ArrayList<>();
     public static String session_email = "";
+    public static String session_ID = "";
     public static HomeShopper.MessageAdapter adapter;
     public static Handler mHandler = new Handler();
     public static String grocery_name = "";
     public static String grocery_image = "";
+
+    public static boolean session_started = false;
+    public static boolean cart_complete = false;
+    public static String isInSession = "false";
 
     private int messageCount = 1;
 
@@ -89,9 +85,24 @@ public class HomeShopper extends AppCompatActivity implements RatingDialogListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_shopper);
 
-        startRepeating();
+        ContentValues params = new ContentValues();
+        params.put("user_email", MainActivity.user_email);
 
-        getSessionEmail();
+        Requests.request(this, "isInSessionShopper", params, response -> {
+            try {
+                // Get the response
+                JSONObject jsonObject = new JSONObject(response);
+                isInSession = jsonObject.getString("inSession");
+
+                if (isInSession.equals("true")) {
+                    getSessionEmail();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+
+        startRepeating();
 
         adapter = new HomeShopper.MessageAdapter();
 
@@ -128,10 +139,18 @@ public class HomeShopper extends AppCompatActivity implements RatingDialogListen
                         fragment = new Shopper_Cart_Fragment();
                         break;
                     case 5:
-                        fragment = new Shopper_Map_Fragment();
+                        if (isInSession.equals("true")) {
+                            fragment = new Shopper_Map_Fragment();
+                        } else {
+                            fragment = new StartASession();
+                        }
                         break;
                     case 6:
-                        fragment = new Shopper_Chat_Fragment();
+                        if (isInSession.equals("true")) {
+                            fragment = new Shopper_Chat_Fragment();
+                        } else {
+                            fragment = new StartASession();
+                        }
                         break;
                     case 7:
                         fragment = new Shopper_Profile_Fragment();
@@ -196,7 +215,8 @@ public class HomeShopper extends AppCompatActivity implements RatingDialogListen
             try {
                 // Get the response
                 JSONObject jsonObject = new JSONObject(response);
-                session_email = jsonObject.getString("session_with");
+                HomeShopper.session_email = jsonObject.getString("user_email");
+                HomeShopper.session_ID = jsonObject.getString("session_ID");
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -214,60 +234,89 @@ public class HomeShopper extends AppCompatActivity implements RatingDialogListen
     private Runnable mHandlerTask = new Runnable() {
         @Override
         public void run() {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    Log.d("TAG", "run");
+            if (!session_started && cart_complete) {
+                ContentValues params = new ContentValues();
+                params.put("user_email", MainActivity.user_email);
+
+                Requests.request(HomeShopper.this, "isInSessionShopper", params, response -> {
                     try {
-                        OkHttpClient client = new OkHttpClient();
-                        HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse("https://lamp.ms.wits.ac.za/~s2430888/checkForMessages.php")).newBuilder();
-                        urlBuilder.addQueryParameter("session_id", "33");
-                        urlBuilder.addQueryParameter("user_email", MainActivity.user_email);
+                        // Get the response
+                        JSONObject jsonObject = new JSONObject(response);
+                        isInSession = jsonObject.getString("inSession");
 
-                        String url = urlBuilder.build().toString();
-
-                        Request request = new Request.Builder()
-                                .url(url)
-                                .build();
-                        Response response = client.newCall(request).execute();
-                        final String result = Objects.requireNonNull(response.body()).string();
-                        // Create json object from the response
-                        JSONObject jsonObject = new JSONObject(result);
-                        runOnUiThread(new Runnable() {
-                                          @Override
-                                          public void run() {
-                                              try {
-                                                  if (jsonObject.getString("found").equals("true")) {
-                                                      String message = jsonObject.getString("msg_content");
-                                                      JSONObject json = new JSONObject();
-                                                      try {
-                                                          json.put("message", message);
-                                                          json.put("byServer", "true");
-
-                                                          // Get the current fragment
-                                                          Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
-                                                          // Check if the current fragment is the chat fragment
-                                                          if (!(currentFragment instanceof Shopper_Chat_Fragment)) {
-                                                              bottomNav.setCount(6, String.valueOf(messageCount++));
-                                                          }
-
-                                                          adapter.addItem(json);
-                                                      } catch (JSONException e) {
-                                                          e.printStackTrace();
-                                                      }
-                                                  }
-                                              } catch (JSONException e) {
-                                                  e.printStackTrace();
-                                              }
-                                          }
-                                      }
-                        );
-                    } catch (Exception e) {
+                        if (isInSession.equals("true")) {
+                            getSessionEmail();
+                            isInSession = "true";
+                            session_started = true;
+                            System.out.println("Found");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(HomeShopper.this, "Session has been accepted", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } else {
+                            System.out.println("Finding...");
+                        }
+                    } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                }
-            }).start();
+                });
+            }
+            if (session_started) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            OkHttpClient client = new OkHttpClient();
+                            HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse("https://lamp.ms.wits.ac.za/~s2430888/checkForMessages.php")).newBuilder();
+                            urlBuilder.addQueryParameter("session_id", HomeShopper.session_ID);
+                            urlBuilder.addQueryParameter("user_email", MainActivity.user_email);
 
+                            String url = urlBuilder.build().toString();
+
+                            Request request = new Request.Builder()
+                                    .url(url)
+                                    .build();
+                            Response response = client.newCall(request).execute();
+                            final String result = Objects.requireNonNull(response.body()).string();
+                            // Create json object from the response
+                            JSONObject jsonObject = new JSONObject(result);
+                            runOnUiThread(new Runnable() {
+                                              @Override
+                                              public void run() {
+                                                  try {
+                                                      if (jsonObject.getString("found").equals("true")) {
+                                                          String message = jsonObject.getString("msg_content");
+                                                          JSONObject json = new JSONObject();
+                                                          try {
+                                                              json.put("message", message);
+                                                              json.put("byServer", "true");
+
+                                                              // Get the current fragment
+                                                              Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+                                                              // Check if the current fragment is the chat fragment
+                                                              if (!(currentFragment instanceof Shopper_Chat_Fragment)) {
+                                                                  bottomNav.setCount(6, String.valueOf(messageCount++));
+                                                              }
+
+                                                              adapter.addItem(json);
+                                                          } catch (JSONException e) {
+                                                              e.printStackTrace();
+                                                          }
+                                                      }
+                                                  } catch (JSONException e) {
+                                                      e.printStackTrace();
+                                                  }
+                                              }
+                                          }
+                            );
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
             mHandler.postDelayed(this, 2000);
         }
     };
